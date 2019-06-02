2b0c558f43f33b2b3082f93282e5ae6b99b1e3db
kasunAtulugama <kasunmalinda17773@gmail.com>
2019-06-02 14:45:54 +0530

    Sementic ui setup

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 
 
-const App = () => <div>
-    <Route path="/" exact component={Home}/>
-    <Route path="/login" exact component={Login}/>
-</div>;
+const App = () =>
+    <div className="ui container">
+        <Route path="/" exact component={Home}/>
+        <Route path="/login" exact component={Login}/>
+    </div>;
 
 export default App;
